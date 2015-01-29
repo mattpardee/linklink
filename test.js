1d74c00c50375a315c7314ebbbdcdc0a3f2db369
Aaron Wilson <aaron@olark.com>
2015-01-29 00:11:09 +0000

    Update to HipChat's more recent code
    This resolves a few edge cases.

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ var linkify = require('./index.js');
 var linkified = linkify.linkify('hello www.bob.com');
 
 console.log(linkified);
+console.log("For actual tests, check out https://github.com/hipchat/linkify/blob/4933af23fec0393ffad2cabfd450436568f70cc1/javascript/tests.html")
